13bed443a5bf62b321ffdcaa5bab404694a3952d
novakkuba <novakkuba102@gmail.com>
2025-03-18 09:59:06 +0000

    initial

---
+++ test_calc.py
@@ -0,0 +1,17 @@
+import pytest
+from calc import Calculator
+
+@pytest.fixture
+def calc():
+    return Calculator()
+def test_add(calc):
+    assert calc.add(2, 3) == 5
+def test_subtract(calc):
+    assert calc.subtract(5, 3) == 2
+def test_multiply(calc):
+    assert calc.multiply(2, 3) == 6
+def test_divide(calc):
+    assert calc.divide(6, 2) == 3
+def test_divide_by_zero(calc):
+    with pytest.raises(ValueError):
+        calc.divide(6, 0)
